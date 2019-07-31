5b48ab8ee4b1e74aea51575e91ad8ac58e6dac55
Niels <nielsrowinbik@users.noreply.github.com>
2019-07-31 21:57:35 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# react-hass-auth
+
+> Home Assistant authentication + React Hooks, with Typescript
+
+[![NPM](https://img.shields.io/npm/v/react-hass-auth.svg)](https://www.npmjs.com/package/react-hass-auth)
+
+Easily add Home Assistant authentication to your React application. This is a thin wrapper over the [home-assistant-js-websocket](https://github.com/home-assistant/home-assistant-js-websocket) library for easily accessing its authentication functionality in your app, with React Context and Hooks. Types are provided.
+
+## Typescript
+
+Library is written in TypeScript. File an issue if you find any problems.
+
+## Install
+
+```bash
+yarn add react-netlify-identity
+```
+
+## Usage
+
+When you call `useHassAuth()`, you can destructure these variables and methods:
+
+-   `authed: boolean | undefined`: if the user is logged in, undefined while loading
+-   `connection: Connection | undefined`: the `home-assistant-js-websocket` connection object once established
+-   `loading: boolean`: if we are determining auth state
+-   `login(hassUrl?: string)`: function to authenticate against provided Home Assistant instance URL (optional if provided through `HassAuthContextProvider`)
+-   `logout()`: logs the user out
+-   `_hassUrl: string | undefined`: the hassUrl used in login method
+
+```tsx
+import React from 'react';
+
+import { HassAuthContextProvider } from 'react-hass-auth';
+
+const App = () => {
+    const url = 'http://hassio.local:8123'; // Supply the url of your Home Assistant instance. If left out, you are required to specify it when calling login.
+
+    return (
+        <HassAuthContextProvider hassUrl={url}>
+            {/* rest of your app */}
+        </HassAuthContextProvider>
+    );
+};
+```
+
+## License
+
+MIT © [nielsrowinbik](https://github.com/nielsrowinbik)
---
+++ src/index.tsx
@@ -0,0 +1,136 @@
+import React, {
+    createContext,
+    ReactNode,
+    SFC,
+    useContext,
+    useEffect,
+    useState,
+} from 'react';
+import {
+    Auth,
+    AuthData,
+    Connection,
+    createConnection,
+    ERR_CANNOT_CONNECT,
+    ERR_INVALID_AUTH,
+    ERR_CONNECTION_LOST,
+    getAuth,
+} from 'home-assistant-js-websocket';
+
+interface HassAuth {
+    authed: boolean | undefined;
+    connection: Connection | undefined;
+    loading: boolean;
+    login: (hassUrl: string) => Promise<Auth>;
+    logout: () => void;
+    _hassUrl: string | undefined;
+}
+
+interface HassAuthContextProviderProps {
+    children: ReactNode;
+    hassUrl?: string;
+}
+
+const [_useHassAuthContext, _HassAuthCtxProvider] = createCtx<HassAuth>();
+
+export const useHassAuth = _useHassAuthContext;
+
+export const HassAuthContextProvider: SFC<HassAuthContextProviderProps> = ({
+    children,
+    hassUrl,
+}: {
+    children: ReactNode;
+    hassUrl?: string;
+}) => {
+    const auth = _useHassAuth(hassUrl);
+    return <_HassAuthCtxProvider value={auth}>{children}</_HassAuthCtxProvider>;
+};
+
+const _useHassAuth = (hassUrl?: string): HassAuth => {
+    const [authed, setAuthed] = useState<boolean | undefined>(undefined);
+    const [connection, setConnection] = useState<Connection | undefined>(
+        undefined
+    );
+    const [_hassUrl, setHassUrl] = useState(hassUrl);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const authenticate = async () => {
+            try {
+                const auth = await getAuth({ loadTokens, saveTokens });
+                const connection = await createConnection({ auth });
+                setHassUrl(auth.data.hassUrl);
+                setConnection(connection);
+                setAuthed(true);
+            } catch (error) {
+                switch (error) {
+                    case ERR_CANNOT_CONNECT:
+                        console.error(
+                            'Unable to connect to the provided Home Assistant host'
+                        );
+                    case ERR_INVALID_AUTH:
+                        console.error('The supplied authentication is invalid');
+                    case ERR_CONNECTION_LOST:
+                        console.error(
+                            'Connection to the Home Assistant host was lost'
+                        );
+                }
+                setAuthed(false);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        authenticate();
+    }, []);
+
+    /*
+     *
+     * OPERATIONS
+     *
+     */
+
+    const login = async (hassUrl: string) => getAuth({ hassUrl });
+
+    const logout = removeTokens;
+
+    return {
+        authed,
+        connection,
+        loading,
+        login,
+        logout,
+        _hassUrl,
+    };
+};
+
+/*
+ *
+ * UTILITY
+ *
+ */
+
+function createCtx<A>() {
+    const ctx = createContext<A | undefined>(undefined);
+    const useCtx = () => {
+        const c = useContext(ctx);
+        if (!c)
+            throw new Error('useCtx must be inside a Provider with a value');
+        return c;
+    };
+    return [useCtx, ctx.Provider] as const;
+}
+
+const saveTokens = (authData: AuthData | null) =>
+    localStorage.setItem('authData', JSON.stringify(authData));
+
+const loadTokens = async () => {
+    const stringified = localStorage.getItem('authData');
+
+    if (stringified === null) return undefined;
+
+    const parsed: AuthData = JSON.parse(stringified);
+    return parsed;
+};
+
+const removeTokens = () => localStorage.removeItem('authData');
